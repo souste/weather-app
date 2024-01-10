2f7b96549ac57017ef0219669c41388eee637728
Stephen <stephensouness@outlook.com>
2024-01-10 15:16:04 +0000

    Successfully fetched data from the Weather API for Manchester

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
-import _ from "lodash";
 import "./style.css";
-import printMe from "./print.js";
+// import printMe from "./print.js";
 
-function component() {
-  const element = document.createElement("div");
-  const btn = document.createElement("button");
-
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  btn.innerHTML = "Click me and check the console!";
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  element.classList.add("hello");
-
-  return element;
-}
-
-document.body.appendChild(component());
+fetch(
+  "https://api.weatherapi.com/v1/forecast.json?key=164214608c0c4ca6b2a112803240901&q=manchester",
+  { mode: "cors" },
+).then((response) => {
+  console.log(response.json());
+});
--- src/print.js
+++ src/print.js
@@ -1,13 +1,3 @@
 export default function printMe() {
   console.log("I get called from print.js!");
-
-  const name = "James";
-
-  const person = { first: name };
-
-  console.log(person);
-
-  const sayHelloLinting = (fName) => {
-    console.log(`Hello linting, ${fName}`);
-  };
 }
