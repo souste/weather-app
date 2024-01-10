e5872e2758c3df3a935f59357828af4f889af3ac
Stephen <stephensouness@outlook.com>
2024-01-10 12:50:30 +0000

    esLint and prettier installed

--- src/print.js
+++ src/print.js
@@ -1,3 +1,13 @@
 export default function printMe() {
   console.log("I get called from print.js!");
+
+  const name = "James";
+
+  const person = { first: name };
+
+  console.log(person);
+
+  const sayHelloLinting = (fName) => {
+    console.log(`Hello linting, ${fName}`);
+  };
 }
