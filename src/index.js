507647d7fdfa8d69d4f93dd172f4eb5cd27188b4
Stephen <stephensouness@outlook.com>
2024-01-10 15:22:06 +0000

    can now fetch nested data within the api object

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./style.css";
 fetch(
   "https://api.weatherapi.com/v1/forecast.json?key=164214608c0c4ca6b2a112803240901&q=manchester",
   { mode: "cors" },
-).then((response) => {
-  console.log(response.json());
-});
+)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response.current.wind_mph);
+  });
