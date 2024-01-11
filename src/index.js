1e0215bc60d284fa90202738f0df1c40bbe06cd3
Stephen <stephensouness@outlook.com>
2024-01-11 09:54:39 +0000

    converted Promise to async/await - getWeatherLocation()

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import "./style.css";
-// import printMe from "./print.js";
 
-fetch(
-  "https://api.weatherapi.com/v1/forecast.json?key=164214608c0c4ca6b2a112803240901&q=manchester",
-  { mode: "cors" },
-)
-  .then((response) => response.json())
-  .then((response) => {
-    console.log(response.current.wind_mph);
-  });
+// const content = document.getElementById("content");
+
+async function getWeatherLocation() {
+  const response = await fetch("https://api.weatherapi.com/v1/forecast.json?key=164214608c0c4ca6b2a112803240901&q=manchester", { mode: "cors" });
+  const weatherData = await response.json();
+  console.log(weatherData.location);
+}
+
+getWeatherLocation();
