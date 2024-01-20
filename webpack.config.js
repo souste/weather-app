1ad230f8e16d99ca0726bc2201e5f025ffb478e0
Stephen <stephensouness@outlook.com>
2024-01-20 13:44:18 +0000

    more changes to get it to work

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
+    filename: "index.bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
     publicPath: "/",
